c850f55c478c908f2a821b53083a9578466368b5
peterziv <peter.ziv@hotmail.com>
2017-11-19 15:49:04 +0800

    [Property Change]Support dropDownList#1 generated by its mode

--- ZSelect2Widget.php
+++ ZSelect2Widget.php
@@ -7,27 +7,29 @@ use yii\base\Widget;
 use yii\helpers\Html;
 
 /**
- * 
+ *
  * This is the 2 level selection control.
  *
- * @property string label Title of the widget.
- * @property array $initList1 It is data of dropdown list #1
- * @property array $initList2 It is initial data of dropdown list #2
- * @property string $postUrl the post url to get new data of dropdown list #2. The default is account/list
- * @property string $dropDownList1Name the element name of dropdown list #1, it also the key post item to refresh data of dropdown list #2. The default is account_type
- * @property \yii\db\ActiveRecord $model (Optional)It is optional. The model object that can be used to render views.
- * @property string $dropDownList2ID if $model exists, it will define the id and name of dropdown list #2 by $model, orwise it is self for the id and name of dropdown list #2. The default is account_id
+ * @property string label                 Title of the widget.
+ * @property string $postUrl              the post url to get new data of dropdown list #2. The default is account/list
+ * @property \yii\db\ActiveRecord $model  (Optional)It is optional. The model object that can be used to render views.
+ * @property array $initList1             It is data of dropdown list #1
+ * @property string $dropDownList1Id      the element name and id of dropdown list #1. If $model exists, it will define the id and name of dropdown list #2 by $model. it also the key post item to refresh data of dropdown list #2. The default is account_type
+ * @property \yii\db\ActiveRecord $model2 (Optional)It is optional. The model object that can be used to render views.
+ * @property array $initList2             It is initial data of dropdown list #2
+ * @property string $dropDownList2Id      if $model2 exists, it will define the id and name of dropdown list #2 by $model2, orwise it is self for the id and name of dropdown list #2. The default is account_id
  *
  * @author Peter (peter.ziv@hotmail.com)
  * @copyright Copyright (c) 2016
- * @example  echo ZSelect2Widget::widget([ 
+ * @example  echo ZSelect2Widget::widget([
  *                     'label' => 'From',
  *                     'formid' => $form->getId(),
- *                     'dropDownList1Name' => 'account_type',
+ *                     'model' => $model,
+ *                     'dropDownList1Id' => 'account_type',
  *                     'postUrl' => 'account/list',
  *                     'initList1' => $accountTypes,
- *                     'model' => $model,
- *                     'dropDownList2ID' => 'account_id',
+ *                     'model2' => $model,
+ *                     'dropDownList2Id' => 'account_id',
  *                     'initList2' => $initAccounts,
  *                 ]);
  */
@@ -35,57 +37,76 @@ class ZSelect2Widget extends Widget {
 
     public $label;
     public $formid; //string
-    public $initList1;
-    public $initList2;
     public $postUrl = 'account/list';
-    public $dropDownList1Name = 'account_type';
-    public $dropDownList2ID = 'account_id';// the dropdownlist #2 name is the same., if $model != null, it will be attribute of dropdown list #2
+
+    //param for dropdown list #1
     public $model = null;
+    public $dropDownList1Id = 'account_type'; // the dropdownlist #1 name is the same., if model != null, it will be attribute of dropdown list #1
+    public $initList1;
+
+    //param for dropdown list #2
+    public $model2 = null;
+    public $dropDownList2Id = 'account_id'; // the dropdownlist #2 name is the same., if model2 != null, it will be attribute of dropdown list #2
+    public $initList2;
 
     public function init() {
         parent::init();
     }
 
     public function run() {
-        $script = Html::tag('div', Html::label($this->label, '', ['class' => "control-label"]), ['class' => 'form-group']);
-        $script.= Html::beginTag('div',['class' => "body-content"]);
-        $script.= Html::beginTag('div', ['class' => "row"]);
-        $script.= Html::beginTag('div', ['class' => "col-lg-5"]);
-        $script.= Html::dropDownList($this->dropDownList1Name, null, $this->initList1, array(
-            'onchange' => $this->changeOnDropDownList1(),
-            'class' => 'form-control',
-            ));
-        $script.= Html::endTag('div');
-        $script.= Html::beginTag('div', ['class' => "col-lg-7"]);
-        $script.= $this->getElementofDropDownList2();
-        $script.= Html::endTag('div');
-        $script.= Html::endTag('div');
-        $script.= Html::endTag('div');
+        $script = Html::beginTag('div', ['class' => 'form-group']);
+        $script .= Html::label($this->label, '', ['class' => "control-label"]);
+        $script .= Html::beginTag('div', ['class' => "body-content"]);
+        $script .= Html::beginTag('div', ['class' => "row"]);
+        $script .= Html::beginTag('div', ['class' => "col-lg-5"]);
+        $script .= $this->getElementofDropDownList1();
+        $script .= Html::endTag('div');
+        $script .= Html::beginTag('div', ['class' => "col-lg-7"]);
+        $script .= $this->getElementofDropDownList2();
+        $script .= Html::endTag('div');
+        $script .= Html::endTag('div');
+        $script .= Html::endTag('div');
+        $script .= Html::endTag('div');
 
         return $script;
     }
-    
+
+    protected function getElementofDropDownList1()
+    {
+        if (is_null($this->model)) {
+            return Html::dropDownList($this->dropDownList1Id, null, $this->initList1, [
+                    'onchange' => $this->changeOnDropDownList1(),
+                    'class' => 'form-control',
+                    'id' => $this->dropDownList2Id
+            ]);
+        }
+        return Html::activeDropDownList($this->model, $this->dropDownList1Id, $this->initList1, [
+                'onchange' => $this->changeOnDropDownList1(),
+                'class' => 'form-control',
+        ]);
+    }
+
     protected function changeOnDropDownList1() {
         return '$.ajax({cache: false,type: "POST",'
-        . 'url:"' . yii::$app->urlManager->createUrl($this->postUrl) . '",'
-        . 'data:$("#' . $this->formid . '").serialize(),'
-        . 'async: false,error: function(request) {alert("Connection error");},'
-        . 'success: function(data) {$("#'.$this->getIDofDropDownList2().'").html(data);}});';
+            . 'url:"' . yii::$app->urlManager->createUrl($this->postUrl) . '",'
+            . 'data:$("#' . $this->formid . '").serialize(),'
+            . 'async: false,error: function(request) {alert("Connection error");},'
+            . 'success: function(data) {$("#' . $this->getIDofDropDownList2() . '").html(data);}});';
     }
 
 
     protected function getIDofDropDownList2(){
-        $id2 = $this->dropDownList2ID;
-        if(!is_null($this->model)){
-            $id2 = Html::getInputId($this->model,  $this->dropDownList2ID);
+        $id2 = $this->dropDownList2Id;
+        if (!is_null($this->model2)) {
+            $id2 = Html::getInputId($this->model2, $this->dropDownList2Id);
         }
         return $id2;
     }
-    
+
     protected function getElementofDropDownList2(){
-        if (is_null($this->model)) {
-            return Html::dropDownList($this->dropDownList2ID, null, $this->initList2, ['class' => 'form-control', 'id' => $this->dropDownList2ID]);
-        } 
-        return Html::activeDropDownList($this->model, $this->dropDownList2ID, $this->initList2, ['class' => 'form-control']);
+        if (is_null($this->model2)) {
+            return Html::dropDownList($this->dropDownList2Id, null, $this->initList2, ['class' => 'form-control', 'id' => $this->dropDownList2Id]);
+        }
+        return Html::activeDropDownList($this->model2, $this->dropDownList2Id, $this->initList2, ['class' => 'form-control']);
     }
 }
